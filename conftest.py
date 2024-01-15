7d892264dec89420d1185d8c2ec91107603b28e2
Николай Титенок <nt@agronote.ru>
2024-01-15 22:23:07 +0300

    Добавлены проверки поиска

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
